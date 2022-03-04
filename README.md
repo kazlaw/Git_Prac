13089d00cf90ab873754b7ea5743d8c329994a87
Kazooba B Lawrence <bitarabehok@gmail.com>
2022-03-04 11:09:52 +0300

    Added clockin python script and modifed README.md
    Added decription to README.md file and python code

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Testing advanced features of git
 
 -practice creating branches and forking and other cool features!!
+
+## .git folder 
+
+- This is hidden but helps the VCS to track changes
---
+++ clockin.py
@@ -0,0 +1,28 @@
+import datetime
+
+class Person:
+    def __init__(self,fname,lname):
+        self.firstname =fname
+        self.lastname=lname
+        
+        
+    def Clockin(self):
+        print(self.firstname , self.lastname + ' came in at ',datetime.datetime.now())
+
+class Salary(Person):
+    def __init__(self, fname,lname ,salo):
+        super().__init__(fname,lname)
+        self.salary =salo
+        salo =int(input('Enter Amount Paid:'))
+
+    def Report(self):
+        print(self.firstname,self.lastname + f' was paid ${self.salary}')
+
+
+test = Salary('kazooba','Lawrence','')
+
+test.Clockin()
+
+print('And')
+
+test.Report()
